d2447cea4425014a30c60af5252ca79183a41aff
Igor Soroka <stupbird@gmail.com>
2018-04-04 13:28:04 +0300

    writing the unit tests

---
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,17 @@
+package guru.springframework.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IndexControllerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getIndexPage() {
+    }
+}
